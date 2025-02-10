c6282adf7e8bbe1b600b2aac8c5c0352fe9ba56e
Haze <yiaocao.andy@gmail.com>
2025-02-10 12:18:48 +0000

    commit

---
+++ APCSA/WellFormed.java
@@ -0,0 +1,14 @@
+public class WellFormed {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.print("A well-formed Java program has ");
+        System.out.print("a main method with { and } ");
+        System.out.println("braces.");
+        System.out.println();
+        System.out.print("A System.out.println statement ");
+        System.out.print("has ( and ) and usually a ");
+        System.out.print("String that starts and ends with a \" character. ");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println();
+    }
+}
